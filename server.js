58e3e1643f2498db566134aa4ce9820c84090c25
Andy Ko <34779522+andyheko@users.noreply.github.com>
2020-06-16 22:38:58 -0700

    Update server.js to build sign-in flow

--- server.js
+++ server.js
@@ -48,32 +48,61 @@ app.get('/', (req, res)=> {
 })
 
 app.post('/signin', (req, res)=> {
-  // // Load hash from your password DB.
-  // bcrypt.compare("bacon", hash, function(err, res) {
-  //     // res == true
-  // });
-  // bcrypt.compare("veggies", hash, function(err, res) {
-  //     // res = false
-  // });
-  if(req.body.email === database.users[0].email &&
-      req.body.password === database.users[0].password){
-    res.json(database.users[0])
-  }else{
-    res.status(400).json('error logging in');
-  }
+  // // // Load hash from your password DB.
+  // // bcrypt.compare("bacon", hash, function(err, res) {
+  // //     // res == true
+  // // });
+  // // bcrypt.compare("veggies", hash, function(err, res) {
+  // //     // res = false
+  // // });
+  // if(req.body.email === database.users[0].email &&
+  //     req.body.password === database.users[0].password){
+  //   res.json(database.users[0])
+  // }else{
+  //   res.status(400).json('error logging in');
+  // }
+  db.select('email', 'hash').from('login')
+    .where('email', '=', req.body.email)
+    .then(user => {
+      const isValid = bcrypt.compareSync(req.body.password, data[0].hash);
+      if (isValid) {
+        db.select('*').from('users')
+          .where('email', '=', req.body.email)
+          .then(user => {
+            res.json(user[0])
+          })
+          .catch(err => res.status(400).json('unable to get user'))
+      } else {
+        res.status(400).json('wrong credentials')
+      }
+    })
+    .catch(err => res.status(400).json('wrong credentials'))
 })
 
 app.post('/register', (req, res)=> {
   const {email, name, password} = req.body;
-  return db('users')
-    .returning('*')
-    .insert({
-      email: email,
-      name: name,
-      joined: new Date()
-    })
-    .then(user => {
-      res.json(user[0]);
+  const hash = bcrypt.hashSync(password);
+    db.transaction(trx => {
+      trx.insert({
+        hash: hash,
+        email: email
+      })
+      .into('login')
+      .returning('email')
+      .then(loginEmail => {
+        return trx('users')
+          .returning('*')
+          .insert({
+            email: loginEmail[0],
+            name: name,
+            joined: new Date()
+          })
+          .then(user => {
+            res.json(user[0]);
+          })
+      })
+      .then(trx.commit)
+      .catch(trx.rollback)
     })
     .catch(err => res.status(400).json('unable to register'))
   // bcrypt.hash(password, null, null, function(err, hash) {
@@ -105,10 +134,10 @@ app.put('/image', (req, res) => {
   .increment('entries', 1)
   .returning('entries')
   .then(entries => {
-    console.log(entries);
+    console.log(entries[0]);
   })
+  .catch(err => res.status(400).json('unable to get entries'))
 })
-
 /*
 / --> res = this is working
 /signin --> POST = success/fail

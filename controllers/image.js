5dcc48d5ff977d028cfb368f7a229dc73dff5ec5
Andy Ko <34779522+andyheko@users.noreply.github.com>
2020-06-18 16:25:31 -0700

    security review
    move clarifai api to backend

--- controllers/image.js
+++ controllers/image.js
@@ -1,3 +1,19 @@
+const Clarifai = require('clarifai');
+
+const app = new Clarifai.App({
+  apiKey: 'd150b398f3bd4e6ca4c25cffa039e9f5'
+});
+
+const handleApiCall = (req, res) => {
+  app.models
+    .predict(Clarifai.FACE_DETECT_MODEL, req.body.input)
+    .then(data => {
+      res.json(data);
+    })
+    .catch(err => res.status(400).json('unalbe to work with API'))
+}
+
+
 const handleImage = (req, res, db) => {
   const { id } = req.body;
   db('users').where('id', '=', id)
@@ -9,5 +25,6 @@ const handleImage = (req, res, db) => {
   .catch(err => res.status(400).json('unable to get entries'))
 }
 module.exports = {
-  handleImage
+  handleImage,
+  handleApiCall
 };

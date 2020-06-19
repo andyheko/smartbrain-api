aa8c017e42404cda345f83782b793f6d05a754de
Andy Ko <34779522+andyheko@users.noreply.github.com>
2020-06-18 20:53:20 -0700

    change response

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-app.get('/', (req, res)=> { res.send(db.users)})
+app.get('/', (req, res)=> { res.send('it is working!')})
 app.post('/signin', (req, res) => { signin.handleSignin(req, res, db, bcrypt)})
 app.post('/register', (req, res) => {register.handleRegister(req, res, db, bcrypt)})
 app.get('/profile/:id', (req, res) => {profile.handleProfileGet(req, res, db)})

87ddd1762f3b35156ac6aa7c6208a052d5c50fc9
Andy Ko <34779522+andyheko@users.noreply.github.com>
2020-07-16 15:58:14 -0700

    add knex

--- server.js
+++ server.js
@@ -25,7 +25,6 @@ const db = knex({
 const app = express();
 
 app.use(cors());
-app.use(bodyParser.json())
 
 app.get('/', (req, res)=> { res.send('it is working!')})
 app.post('/signin', (req, res) => { signin.handleSignin(req, res, db, bcrypt)})

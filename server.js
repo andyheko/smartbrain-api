bb6bff4a731fd083a2107e300d6b5baa8fd52371
Andy Ko <34779522+andyheko@users.noreply.github.com>
2020-07-16 16:40:24 -0700

    update database

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const image = require('./controllers/image');
 const db = knex({
   client: 'pg',
   connection: {
-    host : '127.0.0.1',
+    host : 'postgresql-closed-13842',
     user : 'andyko',
     password : '',
     database : 'smart-brain'

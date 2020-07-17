1f45a3c5a882b837b63c00e8682736ea27c4d91f
Andy Ko <34779522+andyheko@users.noreply.github.com>
2020-07-16 21:16:12 -0700

    debug server.js

--- controllers/register.js
+++ controllers/register.js
@@ -3,7 +3,8 @@ const handleRegister = (req, res, db, bcrypt)=> {
   if (!email || !name || !password) {
     return res.status(400).json('incorrect form submission');
   }
-  const hash = bcrypt.hashSync(password);
+  const salt = bcrypt.genSaltSync(saltRounds);
+  const hash = bcrypt.hashSync(password, salt);
     db.transaction(trx => {
       trx.insert({
         hash: hash,

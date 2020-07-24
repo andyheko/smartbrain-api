92de59fabffeaffc73d499d0e480c0cbb0540b57
Andy Ko <34779522+andyheko@users.noreply.github.com>
2020-07-24 16:00:15 -0700

    change line 28 in server.js

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ const app = express();
 app.use(cors());
 
 app.get('/', (req, res)=> { res.send('it is working!')})
-app.post('/signin', (req, res) => {signin.handleSignin(req, res, db, bcrypt)})
+app.post('/signin', {signin.handleSignin(req, res, db, bcrypt)})
 app.post('/register', (req, res) => {register.handleRegister(req, res, db, bcrypt)})
 app.get('/profile/:id', (req, res) => {profile.handleProfileGet(req, res, db)})
 app.put('/image', (req, res) => {image.handleImage(req, res, db)})

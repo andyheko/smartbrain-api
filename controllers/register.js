16b85f851b24bfd0336cf8304b1929ca35dbf3d8
Andy Ko <34779522+andyheko@users.noreply.github.com>
2020-07-24 17:11:07 -0700

    update register.js

--- controllers/register.js
+++ controllers/register.js
@@ -1,5 +1,4 @@
 const handleRegister = (req, res, db, bcrypt) => {
-  console.log(req.body)
   const { email, name, password } = req.body;
   if (!email || !name || !password) {
     return res.status(400).json('incorrect form submission');

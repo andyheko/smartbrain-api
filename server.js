e88b6b2324517aafa419c40af465bfbe6f09b6a2
Andy Ko <34779522+andyheko@users.noreply.github.com>
2020-07-28 11:59:57 -0700

    update register

--- server.js
+++ server.js
@@ -9,23 +9,23 @@ const signin = require('./controllers/signin');
 const profile = require('./controllers/profile');
 const image = require('./controllers/image');
 
-// const db = knex({
-//   client: 'pg',
-//   connection: {
-//     connectionString : process.env.DATABASE_URL,
-//     ssl : true
-//   }
-// });
-
 const db = knex({
   client: 'pg',
   connection: {
-    host : '127.0.0.1',
-    user : 'andyko',
-    password : '',
-    database : 'smart-brain'
+    connectionString : process.env.DATABASE_URL,
+    ssl : true
   }
 });
+
+// const db = knex({
+//   client: 'pg',
+//   connection: {
+//     host : '127.0.0.1',
+//     user : 'andyko',
+//     password : '',
+//     database : 'smart-brain'
+//   }
+// });
 //console.log(db.select('*').from('users'))
 
 // db.select('*').from('users').then(data => {

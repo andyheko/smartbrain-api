9c849d6b35de537ed682ebab80fcc6b6289d8008
Andy Ko <34779522+andyheko@users.noreply.github.com>
2020-07-28 13:30:49 -0700

    debug unable to work with api

--- controllers/image.js
+++ controllers/image.js
@@ -10,7 +10,7 @@ const handleApiCall = (req, res) => {
     .then(data => {
       res.json(data);
     })
-    .catch(err => res.status(400).json('unalbe to work with API'))
+    .catch(err => res.status(400).json(err))
 }
 
 
